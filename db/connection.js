6826500623ca7689f9070d6b49116f230a7c2118
Lewis Botcherby <lewisbotcherby@gmail.com>
2022-12-15 11:49:44 +0000

    minor changes to connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,21 +5,25 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+	throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = ENV === 'production' ? {
-  connectionString: process.env.DATABASE_URL,
-  max: 2,
-  user: 'lewis',
-  host: 'localhost',
-  password: 'password',
-  port: 5432
-} : { user: 'lewis',
-host: 'localhost',
-password: 'password',
-port: 5432};
+
+const config =
+	ENV === 'production'
+		? {
+				connectionString: process.env.DATABASE_URL,
+				max: 2,
+        user: 'lewis',
+        host: 'localhost',
+        password: 'password',
+        port: 5432
+		  }
+		: {  user: 'lewis',
+    host: 'localhost',
+    password: 'password',
+    port: 5432};
 
 
 const securityDetails = {
