387425eb76f3bfc33a947df786549bb6a13d17d2
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-02-10 02:45:25 +0000

    added emails and passwords to dev data and created auth middleware

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -4,35 +4,47 @@ module.exports = [
     name: 'Tom Tickle',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
+    email: "tom@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
+    email: "paul@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
     avatar_url:
       'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    email: "amy@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
+    email: "peter@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
+    email: "gemma@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
+    email: "jess@gmail.com",
+    password: "$2a$10$dDJJJB6yk.OlpToYjts9VO3kUpBeMhQuMR4yLkMjXhAB78NOtlZG."
   },
 ];
